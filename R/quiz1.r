642be06d0b03d6760932672b3e508dcf37df608f
GPC6 <100397982+GPC6@users.noreply.github.com>
2024-09-02 03:07:26 +0900

    모델빼고 다함

---
+++ R/quiz1.r
@@ -0,0 +1,59 @@
+library(GenderInfer)
+library(nycflights13)
+library(tidyverse)
+
+
+planes
+list= planes %>% group_by(manufacturer) %>% summarise(count=n()) %>% filter(count>200)
+cut_planes = planes %>% filter(manufacturer %in% list$manufacturer, !is.na(year)) %>% group_by(manufacturer, year) %>% summarise(made=n())
+
+tail(arrange(cut_planes,year))
+cut_planes %>% 
+ggplot(aes(year,made, color=manufacturer)) +
+geom_line() +
+geom_point() +
+theme(aspect.ratio = 1/2) +
+xlim(1985,2013)
+
+#답안
+planes %>% group_by(manufacturer) %>% mutate(n=n()) %>% filter(n>200) %>%
+ggplot(aes(x=year)) +
+geom_freqpoly(aes(color=manufacturer), binwidth=10)
+# 그룹화 + 뮤테이트 = 내가 원하는 그거 (그룹 요소수 세기)
+
+
+
+
+authors_df <- assign_gender(data_df = authors, first_name_col = "first_name")
+authors_df %>% 
+    group_by(gender, publication_years) %>% 
+    group_by(gender) %>%
+    mutate(count=n()) %>%
+    filter(gender!="U") %>%
+    group_by(gender, publication_years) %>%
+    summarise(publicaiton_count = n(), count=first(count)) %>%
+    arrange(gender, desc(publicaiton_count)) %>%
+    summarise(most_published_year=first(publication_years),count=first(count))
+
+
+#답안
+temp=authors_df %>%
+    filter(gender != "U") %>%
+    group_by(gender) %>%
+    count(publication_years)
+    summarise(most_published_year = publication_years[rank(n)==length(n)],count=sum(n))
+#미친문법 count함수와 rank와 length를 이용해 1등만 골라먹기 하는 미친 테크닉
+#count = 그룹핑 두개 + 요약
+
+
+jobs=read_csv("F:\\data\\NYC_Jobs-1.csv")
+
+jobs=jobs %>% 
+    select(`Job Category`, `Salary`) %>% 
+    filter(`Job Category` %in% c("Finance, Accounting, & Procurement", "Technology, Data & Innovation", "Health"), `Salary`>10000)
+
+
+jobs %>%
+    ggplot(aes(x=`Job Category`, y=`Salary`, color=`Job Category`)) +
+    geom_boxplot() +
+    geom_point(data= jobs %>% filter(`Salary`>200000), shape="triangle", size=3)
---
+++ R/quiz2.r
@@ -0,0 +1,104 @@
+library(nycflights13)
+library(tidyverse)
+library(stringr)
+
+holidays=read_csv("F:\\data\\holiday.csv")
+
+holidays %>%
+    filter(str_detect(name, "Birthday")) %>%
+    summarise(num_birthday = n())
+
+federal_flight = holidays %>%
+    filter(type=="Federal Holiday") %>%
+    left_join(flights)
+
+federal_flight
+
+federal_flight %>%
+    select(name, tailnum) %>%
+    group_by(name) %>%
+    summarise(n=n()) %>%
+    arrange(desc(n))
+
+
+raw_mbti = read_table("F:\\data\\raw_MBTI.txt",col_names = FALSE)
+data=tibble(
+    mbti_pair=str_split(raw_mbti[[1]],",")[[1]]
+    ) 
+
+data2=summarise(
+    data, 
+    Man=str_sub(mbti_pair, 1,4), 
+    Woman=str_sub(mbti_pair, -4,-1)
+    )
+
+mbti=c("ISTJ","ISTP","ESTP","ESTJ","ISFJ","ISFP","ESFP","ESFJ","INFJ","INFP","ENFP","ENFJ","INTJ","INTP","ENTJ","ENTP")
+
+couple_MBTI = data2 %>%
+    summarise(
+        Man = factor(Man, levels=mbti),
+        Woman = factor(Woman,levels=mbti)
+        )
+
+head(couple_MBTI,5)
+
+couple_MBTI %>%
+    count(Man, Woman) %>%
+    ggplot(aes(Man, Woman)) +
+    geom_tile(aes(fill=n)) +
+    theme(aspect.ratio = 1/1)
+
+counted_mbti=couple_MBTI %>%
+    count(Man, Woman) %>%
+    mutate(matched = is.na(Man)*1)
+
+
+str_sub(counted_mbti[[i, 1]],j,j)  == str_sub(counted_mbti[[i, 2]],j,j)
+for(i in 1:256)
+{
+    n=0
+    for(j in 1:4)
+    {
+        n = n + (str_sub(counted_mbti[[i, 1]],j,j) == str_sub(counted_mbti[[i, 2]],j,j))*1
+    }
+    counted_mbti$matched[i]=n
+} #상남자식 반복문
+
+counted_mbti %>% 
+    group_by(matched) %>%
+    summarise(n=sum(n))
+
+
+#답안
+couple_MBTI %>%
+    rowwise() %>%
+    mutate(matched = length(intersect(unlist(str_split(Man,"")), unlist(str_split(Woman,""))))) %>%
+    group_by(matched) %>%
+    summarise(n=n())
+
+#rowwise 함수는 각 행을 다 그룹화 시킨다
+#마치 행별로 전부 브로드캐스팅하는 느낌
+
+
+starwars %>%
+    select(name) %>%
+    filter(!is.na(parse_number(name)))
+
+is_vowel=starwars %>%
+    select(name) %>%
+    mutate(vowel=str_count(name,"[aeiouAEIOU]")) %>%
+    arrange(vowel)
+is_vowel
+
+is_vowel %>%
+    group_by(vowel) %>%
+    summarise(mean_vowel = mean(str_length(name)))
+
+#답안 (패턴만들기)
+
+num=c(as.character(0:9)) #문자열
+num_pattern=str_c(num,collapse = "|")
+starwars %>%
+    select(name) %>%
+    filter(str_detect(name,num_pattern))
+
